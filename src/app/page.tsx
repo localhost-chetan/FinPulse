7090c5b71fb9d0bdc28bf3b8ac4747332f7b19b4
Chetan Seervi <chetanseervi513@gmail.com>
2025-03-10 20:31:21 +0530

    Add shadcn-ui library

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,11 @@
+import { Button } from "@shadcn/button";
 import { APP_NAME } from "@config/appConfig";
 
 export default function Home() {
-  return <div>{`Welcome to ${APP_NAME}`}</div>;
+  return (
+    <div>
+      <h1>{`Welcome to ${APP_NAME}`}</h1>
+      <Button>Subscribe Now</Button>
+    </div>
+  );
 }
